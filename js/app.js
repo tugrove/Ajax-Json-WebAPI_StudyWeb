d647129e3630f235f374ee3ceceef9c235e9a7bd
tugrove <asmallpondinalargecastle@gmail.com>
2021-02-15 13:14:59 +0900

    [add]refs #5 uri結合用の関数を追加

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// URIを結合するための関数
+const urijoin = (strings, ...values) => {
+    const result = [];
+    const I = strings.length - 1;
+    for (let i = 0; i < I; i++) {
+        result.push(strings[i], encodeURIComponent(values[i]));
+    }
+    result.push(strings[I]);
+    return result.join(``);
+};
