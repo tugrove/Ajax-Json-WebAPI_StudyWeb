51ffd9ffbac8e900126bbeaa8bcc7cf59143bda4
tugrove <asmallpondinalargecastle@gmail.com>
2021-02-16 09:45:45 +0900

    [add]refs #5 郵便番号から住所を検索できる機能を追加（with jQuery）

--- js/app.js
+++ js/app.js
@@ -1,3 +1,33 @@
+// HTMLを読み込んでから処理
+$(
+    // 検索ボタンをクリックした時の処理
+    $(`#btn`).on(`click`, () => {
+        const ZIPCODE = $(`#zipcode`).val();
+        const URL = urijoin`https://zipcloud.ibsnet.co.jp/api/search?zipcode=${ZIPCODE}`;
+        $.ajax({
+            url: URL,
+            dataType: `jsonp`
+        }).done( data => {
+            // 通信に成功した時の処理
+            if (data.results) {
+                // 取得したデータを各HTML要素に代入
+                const ADDRESS = data.results[0];
+                $(`#prefecture`).val(ADDRESS.address1); // 都道府県を代入
+                $(`#city`).val(ADDRESS.address2);       // 市区町村を代入
+                $(`#address`).val(ADDRESS.address3);    // 住所を代入
+            } else {
+                // データが取得できなかった時の処理
+                console.log(data);
+                alert(`該当するデータが見つかりませんでした`);
+            }
+        }).fail( data => {
+            // 通信に失敗した時の処理
+            alert(`通信に失敗しました`);
+        });
+    })
+);
+
+/* 関数定義 ---------------------------------- */
 // URIを結合するための関数
 const urijoin = (strings, ...values) => {
     const result = [];
