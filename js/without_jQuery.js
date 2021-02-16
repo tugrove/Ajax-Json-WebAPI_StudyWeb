7ab06d51b6f3fdfccf963515aa212e08a1b35b58
tugrove <asmallpondinalargecastle@gmail.com>
2021-02-16 10:59:34 +0900

    [add]refs #7 ボタンを押した時にAPI用script要素を追加する機能を追加（without jQuery）
    郵便番号を入力してボタンを押すと、script要素を追加して即削除する

--- js/without_jQuery.js
+++ js/without_jQuery.js
@@ -1,3 +1,19 @@
+// HTML要素の取得
+const BTN = document.getElementById(`btn`);
+const DIV_JS = document.getElementById(`js`);
+
+// ボタンが押された時の処理
+BTN.addEventListener(`click`, () => {
+    // API用のURLを作成
+    const ZIPCODE = document.getElementById(`zipcode`).value;
+    const URL = urijoin`https://zipcloud.ibsnet.co.jp/api/search?zipcode=${ZIPCODE}&callback=${cb_address.name}`;
+    // API用のscript要素を追加して削除
+    const SCRIPT_API = document.createElement(`script`);
+    SCRIPT_API.type = `text/javascript`
+    SCRIPT_API.src = URL;
+    DIV_JS.insertBefore(SCRIPT_API, null);
+    DIV_JS.removeChild(SCRIPT_API);
+});
 
 
 /* 関数定義 ---------------------------------- */
@@ -11,3 +27,8 @@ const urijoin = (strings, ...values) => {
     result.push(strings[I]);
     return result.join(``);
 };
+
+// 住所検索APIのコールバック関数
+const cb_address = data => {
+    console.log(data);
+}
